b523cd671c7ac67c27ccc46e238db9920174d53b
EstebanDiaz43 <esteban.demiljo@gmail.com>
2025-06-17 09:59:19 +0200

    Finished Challenge 5-h

--- Challenge 4/Challenge g/script.js
+++ Challenge 4/Challenge g/script.js
@@ -5,8 +5,8 @@ const NONE_SELECTED = 0;
 
 const customers = 1;
 const location = "RSA";
-let shipping = null;
 let currency = null;
+let shipping = null;
 
 const shoes = 300 * 1;
 const toys = 100 * 5;
@@ -14,29 +14,26 @@ const shirts = 150 * NONE_SELECTED;
 const batteries = 35 * 2;
 const pens = 5 * NONE_SELECTED;
 
-const total = shoes + toys + shirts + batteries + pens;
-
 if (location === "RSA") {
-  shipping === 400;
-  currency === "R";
-} else if ((location = "NAM")) {
-  shipping = 600;
-  currency = "$";
+  currency = "R";
+  shipping = 400;
 } else {
-  shipping = 800;
   currency = "$";
+  shipping = location === "NAM" ? 600 : 800;
 }
 
-if (total > 1000) {
-  if (location === "NAM" && customers === 1) {
-    if (location === "RSA") shipping = 0;
-  }
-}
+const total = shoes + batteries + pens + shirts + toys;
+const isFreeLocal = currency === "R" && total > 1000;
+const isFreeInt = currency === "$" && total > 60;
+const isFree = isFreeLocal || isFreeInt;
+const isFreeWarning = isFree && customers !== 1;
+const totalWithShipping = total + shipping;
+const isBanned = location === "NK";
 
-if ((shipping = 0 && customers !== 1)) {
-  console.log(FREE_WARNING);
-}
+if (isFreeWarning) console.log(FREE_WARNING);
+if (isFree && !isFreeWarning) shipping = 0;
 
-location === "NK"
-  ? console.log(BANNED_WARNING)
-  : console.log("price", currency, total + shipping);
+const withShipping = total + shipping;
+console.log(
+  isBanned ? BANNED_WARNING : "Price: " + currency + totalWithShipping
+);
--- Challenge 5/Challenge h/script.js
+++ Challenge 5/Challenge h/script.js
@@ -1,11 +1,10 @@
-const primaryPhone = 'O748105141'
-const secondaryPhone = '0219131568'
+const primaryPhone = "O748105141";
+const secondaryPhone = "0219131568";
 
 // Only change below this line
 
-const primaryValid = typeof number == primaryPhone 
-const secondaryValid = typeof number == secondaryPhone
+const primaryValid = !Number.isNaN(parseInt(primaryPhone));
+const secondaryValid = !Number.isNaN(parseInt(secondaryPhone));
 
-console.log('Primary phone is valid numerical string:', primaryValid)
-console.log('Secondary phone is valid numerical string:', secondaryValid )
- 
+console.log("Primary phone is valid numerical string:", primaryValid);
+console.log("Secondary phone is valid numerical string:", secondaryValid);
